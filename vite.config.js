8acc7cc3b17cc8f08eb9cdfc36f68648f4e69180
mo2men165 <moamenabdeltawab@gmail.com>
2023-12-16 19:52:02 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/3D-Portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
